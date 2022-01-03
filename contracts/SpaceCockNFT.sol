b370f4bd40ae6d5710e526008ca6dc056e0c0df9
Gerhard Molin <gerhard@minimumbadass.com>
2022-01-03 15:02:16 +0100

    basic smart contract + test file done

---
+++ contracts/SpaceCockNFT.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract SpaceCockNFT {
+	constructor() {
+		console.log('This is my NFT contract.');
+	}
+}
