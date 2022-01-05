b772f9192bac555d28c47d3dc8b5beeb2307d750
Gerhard Molin <gerhard@minimumbadass.com>
2022-01-05 12:56:55 +0100

    minting NFT through site works

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from "react";
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
+import { ethers } from "ethers";
+import config from './config/config';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -11,6 +13,7 @@ const TOTAL_MINT_COUNT = 50;
 const App = () => {
 
 	const [currentAccount, setCurrentAccount] = useState('');
+	const [isMinting, setIsMinting] = useState(false);
 
 
 	// Here we check if a wallet is connected
@@ -57,7 +60,7 @@ const App = () => {
 				console.log('no authorized account found');
 			} 
 		} catch (error) {
-			
+			console.log(error);
 		}
 	}
 
@@ -65,11 +68,29 @@ const App = () => {
 	* Implement your connectWallet method here
 	*/
 	const askContractToMintNft = async () => {
+		setIsMinting(true);
 		try {
-			const { ethereum } = window
+			const { ethereum } = window;
+
+			if (ethereum) { 
+				const provider = new ethers.providers.Web3Provider(ethereum);
+				const signer = provider.getSigner();
+				const connectedContract = new ethers.Contract(config.contractAddress, config.contractABI, signer);
+
+				console.log("Going to pop wallet now to pay gas...")
+				// Mint an NFT :)
+				const nftTxn = await connectedContract.makeAnEpicNFT();
+				console.log("Mining...please wait.")
+				await nftTxn.wait();
+				console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+
+			} else {
+				window.alert("Ethereum object doesn't exist!");
+			}
 		} catch (error) {
-			
+			console.log(error);
 		}
+		setIsMinting(true);
 	}
 
 	// Render Methods
@@ -81,7 +102,7 @@ const App = () => {
 
 	// Render Mint Button
 	const renderMintContainer = () => (
-		<button onClick={connectWallet}  className="cta-button connect-wallet-button">
+		<button onClick={askContractToMintNft}  className="cta-button connect-wallet-button">
 			Mint NFT
 		</button>
 	);
--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,8 +1,8 @@
 import abi from '../utils/SpaceCockNFT.json';
 import dotenv from 'dotenv';
-
 dotenv.config();
-const contractAddress = process.env.SMART_CONTRACT_ADDRESS;
+
+const contractAddress = '0x238f47c35619076f6B8432416B04B67aaCF678F4';
 const contractABI = abi.abi;
 
 export default {
